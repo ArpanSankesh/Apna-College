017c1c15f8342a7759de53aae87013faec96ceff
ArpanSankesh <arpansankesh830@gmail.com>
2024-03-01 19:24:35 +0530

    Find

--- mongo/inxed.js
+++ mongo/inxed.js
@@ -1,31 +1,41 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-main().then((res) => {
+main()
+  .then((res) => {
     console.log("connetion successful");
-})
-.catch(err => console.log(err));
+  })
+  .catch((err) => console.log(err));
 
-async function main(){
-    await mongoose.connect('mongodb://127.0.0.1:27017/test');
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/test");
 }
 
 const userSchema = new mongoose.Schema({
-    user: String, 
-    email:String,
-    age: Number 
+  user: String,
+  email: String,
+  age: Number,
 });
 
 const user = mongoose.model("user", userSchema);
 
-const user1 = new user({
-    name:"Adam",
-    email:"adam@gmail.com",
-    age:"18"
-})
-
-user1.save().then((res) => {
+user
+  .find({age:{$gt: 16} })
+  .then((res) => {
     console.log(res);
-})
-.catch((err) => {
+  })
+  .catch((err) => {
     console.log(err);
-});
+  });
+
+// const user1 = new user({
+//     name:"Adam",
+//     email:"adam@gmail.com",
+//     age:"18"
+// })
+
+// user1.save().then((res) => {
+//     console.log(res);
+// })
+// .catch((err) => {
+//     console.log(err);
+// });
